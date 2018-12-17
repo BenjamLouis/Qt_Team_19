944ab56cf3c90b5d90ea0df479607bacdad5e61b
Louis Benjamin Bruno Vincent <blouis@INTRANET.EPFL.CH@CO-IN-SC-229.intranet.epfl.ch>
2018-12-17 15:01:21 +0100

    Bonsoir

--- GUI_Genomics/mainwindow.cpp
+++ GUI_Genomics/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include <QPixmap>
 #include <QPalette>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -14,4 +15,18 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    QString MatrixfileName = QFileDialog::getOpenFileName(this,
+        tr("Open Matrix File"), "", tr("Matrix Files (*.mat)"));
+    ui->pushButton_3->setText(MatrixfileName);
+}
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    QString FastafileName = QFileDialog::getOpenFileName(this,
+        tr("Open Fasta File"), "", tr("Fasta Files (*.fa *.fasta)"));
 }
