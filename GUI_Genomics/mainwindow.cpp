38f23267c383a1261fb7410b309340f796ecd6cb
Louis Benjamin Bruno Vincent <blouis@INTRANET.EPFL.CH@CO-IN-SC-205.intranet.epfl.ch>
2018-12-19 14:51:11 +0100

    Tu coco les bails

--- GUI_Genomics/mainwindow.cpp
+++ GUI_Genomics/mainwindow.cpp
@@ -3,6 +3,7 @@
 #include <QPixmap>
 #include <QPalette>
 #include <QFileDialog>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -29,4 +30,60 @@ void MainWindow::on_pushButton_4_clicked()
 {
     QString FastafileName = QFileDialog::getOpenFileName(this,
         tr("Open Fasta File"), "", tr("Fasta Files (*.fa *.fasta)"));
+}
+
+
+
+
+
+//-------------- Tab2 ------------------
+
+
+void MainWindow::on_FastaButton_Tab2_clicked()
+{
+    FastafileName_Tab2 = QFileDialog::getOpenFileName(this,
+        tr("Open Fasta File"), "", tr("Fasta Files (*.fa *.fasta)"));
+    ui->FastaButton_Tab2->setText(FastafileName_Tab2);
+}
+
+void MainWindow::on_BedButton_clicked()
+{
+    BedfileName = QFileDialog::getOpenFileName(this,
+        tr("Open Bed File"), "", tr("Bed Files (*.bed)"));
+    ui->BedButton->setText(BedfileName);
+}
+
+
+void MainWindow::on_ButtonFunction2_clicked()
+{
+    // Bouton depart programme
+
+    QString errors("Missing: \n");
+    bool error_arguments;
+    if ((ui->BedButton->text() == "Browse Files") or (ui->BedButton->text() == "") or (ui->BedButton->text() == "Please Select a file")) {
+        ui->BedButton->setText("Please Select a file");
+        errors += "\t-Bed File \n";
+        error_arguments = true;
+    };
+
+    if ((ui->FastaButton_Tab2->text() == "Browse Files") or (ui->FastaButton_Tab2->text() == "") or (ui->FastaButton_Tab2->text() == "Please Select a file")) {
+        ui->FastaButton_Tab2->setText("Please Select a file");
+        errors += "\t-Fasta File \n";
+        error_arguments = true;
+    };
+
+    if (ui->LengthLine->text() == "") {
+        errors += "\t-Length \n";
+        error_arguments = true;
+
+    }
+
+    if (error_arguments) {
+
+       errors += "\nPlease enter the missing information";
+       QMessageBox::warning(this, tr("Missing Information(s)"),
+                               errors,
+                               QMessageBox::Ok);
+    }
+
 }
--- GUI_Genomics/mainwindow.h
+++ GUI_Genomics/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <array>
 
 namespace Ui {
 class MainWindow;
@@ -22,8 +23,18 @@ private slots:
 
     void on_pushButton_4_clicked();
 
+    void on_FastaButton_Tab2_clicked();
+
+    void on_BedButton_clicked();
+
+    void on_ButtonFunction2_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QString FastafileName_Tab2;
+    QString BedfileName;
+
+
 };
 
 #endif // MAINWINDOW_H
